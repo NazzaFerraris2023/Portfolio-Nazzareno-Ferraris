3cc76e818725c90908fa82c81c2eb0d660e6191c
Nazzareno Ferraris Sabbatini <fnazzareno@yahoo.com.ar>
2026-02-23 19:16:46 -0300

    connecting back with front

--- components/ContactComponent.jsx
+++ components/ContactComponent.jsx
@@ -70,7 +70,7 @@ const ContactComponent = () => {
     if (honeyRef.current.value) return;
     try {
       const res = await fetch(
-        "http://localhost:8080/nodemailer",
+        "https://portfolio-back-wqtk.onrender.com/nodemailer",
         {
           method: "POST",
           headers: {
